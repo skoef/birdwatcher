ec819f98e29cadaf467b64961b11a3fa45cfb317
Reinier Schoof <rschoof@transip.nl>
2020-02-27 15:33:20 +0100

    fixed testing of writing bird config

--- birdwatcher/bird_test.go
+++ birdwatcher/bird_test.go
@@ -1,13 +1,12 @@
 package birdwatcher
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"net"
 	"os"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestWriteBirdConfig(t *testing.T) {
@@ -17,8 +16,11 @@ func TestWriteBirdConfig(t *testing.T) {
 	require.NoError(t, err)
 	defer os.Remove(tmpFile.Name())
 
+	prefixes := make(PrefixCollection)
+	prefixes["match_route"] = NewPrefixSet("match_route")
+
 	// write bird config with empty prefix list
-	err = writeBirdConfig(tmpFile.Name(), "match_route", []net.IPNet{})
+	err = writeBirdConfig(tmpFile.Name(), "match_route", prefixes)
 	require.NoError(t, err)
 
 	// read data from temp file and compare it to file fixture
@@ -30,13 +32,13 @@ func TestWriteBirdConfig(t *testing.T) {
 
 	assert.Equal(t, fixture, data)
 
+	prefixes["match_route"].Add(net.IPNet{IP: net.IP{1, 2, 3, 4}, Mask: net.IPMask{255, 255, 255, 255}})
+	prefixes["match_route"].Add(net.IPNet{IP: net.IP{2, 3, 4, 5}, Mask: net.IPMask{255, 255, 255, 192}})
+	prefixes["match_route"].Add(net.IPNet{IP: net.IP{3, 4, 5, 6}, Mask: net.IPMask{255, 255, 255, 0}})
+	prefixes["match_route"].Add(net.IPNet{IP: net.IP{4, 5, 6, 7}, Mask: net.IPMask{255, 255, 248, 0}})
+
 	// write bird config to it
-	err = writeBirdConfig(tmpFile.Name(), "match_route", []net.IPNet{
-		{IP: net.IP{1, 2, 3, 4}, Mask: net.IPMask{255, 255, 255, 255}},
-		{IP: net.IP{2, 3, 4, 5}, Mask: net.IPMask{255, 255, 255, 192}},
-		{IP: net.IP{3, 4, 5, 6}, Mask: net.IPMask{255, 255, 255, 0}},
-		{IP: net.IP{4, 5, 6, 7}, Mask: net.IPMask{255, 255, 248, 0}},
-	})
+	err = writeBirdConfig(tmpFile.Name(), PrefixFamilyIPv4, prefixes)
 	require.NoError(t, err)
 
 	// read data from temp file and compare it to file fixture
